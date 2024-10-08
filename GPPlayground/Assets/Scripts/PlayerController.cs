56206e6c5a23c2937b12b589f74c2dd90eca3570
trush <trushchakolga@gmail.com>
2024-10-08 17:32:21 +0300

    add direct control for player character

--- GPPlayground/Assets/Scripts/PlayerController.cs
+++ GPPlayground/Assets/Scripts/PlayerController.cs
@@ -1,31 +1,59 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] Vector3 target;
-    
-    NavMeshAgent agent;
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] private InputActionAsset actions;
+    private InputAction pointAction;
+    private Vector2 moveInput;
+    [SerializeField] private float directVelocity;
+    private Rigidbody2D rb;
     
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
-        //target = agent.transform.position;
-        //agent.SetDestination(target);
+        //actions = GetComponent<PlayerInput>().actions;
+        //pointAction = actions.FindAction("Fire");
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(1))
+        // if (pointAction.WasPerformedThisFrame())
+        // {
+        //     Debug.Log("Mouse down");
+        // }
+        rb.velocity = moveInput * directVelocity;
+        if (agent.enabled && Math.Abs(target.x - transform.position.x) <= 0.1 && Math.Abs(target.y - transform.position.y) <= 0.1)
         {
-            //Debug.Log("Mouse down");
-            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            agent.SetDestination(target);
+            Debug.Log("Reached destination");
+            agent.isStopped = true;
         }
-        //Debug.Log(target);
+    }
+
+    public void SetTarget(InputAction.CallbackContext context)
+    {
+        agent.enabled = true;
+        target = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        target.z = 0;
+        agent.SetDestination(target);
+        agent.isStopped = false;
+        //agent.SetDestination(new Vector3(0,0,0));
+
+
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        agent.enabled = false;
+        moveInput = context.ReadValue<Vector2>();
     }
 }
